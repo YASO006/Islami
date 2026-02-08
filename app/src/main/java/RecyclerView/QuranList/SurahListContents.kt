d4dcc3e7386bafe76efab22dbcf6d0c8e67d86ce
Yassin Yasser <yassin7002233@gmail.com>
2026-02-08 13:58:48 +0200

    quran list finished

---
+++ app/src/main/java/RecyclerView/QuranList/SurahListContents.kt
@@ -0,0 +1,5 @@
+package RecyclerView.QuranList
+
+data class SurahListContents(var surahName : String,var versesCount : String, var surahNameInArabic : String,var SurahNumber : String) {
+
+}
---
+++ app/src/main/java/com/example/islami/ui/screens/main/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.islami.ui.screens.main
+
+import RecyclerView.QuranList.MyAdapter
+import RecyclerView.QuranList.SurahListContents
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.islami.R
+import com.example.islami.ui.screens.fragments.hadith.HadithFragment
+import com.example.islami.ui.screens.fragments.quran.QuranFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        initViews()
+        onClickItem()
+
+    }
+
+
+
+    private fun initViews() {
+        button = findViewById(R.id.navigationBar)
+    }
+
+    private fun onClickItem() {
+        button.setOnItemSelectedListener {
+            item ->
+            when(item.itemId){
+
+                R.id.quranTab -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, QuranFragment())
+                        .addToBackStack(null)
+                        .commit()
+                }
+                R.id.hadithTab -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, HadithFragment())
+                        .addToBackStack(null)
+                        .commit()
+
+                }
+
+                R.id.radioTab -> {
+                    Toast.makeText(baseContext,"Todo Task",Toast.LENGTH_SHORT).show()
+                    return@setOnItemSelectedListener false
+                }
+                R.id.sebhaTab -> {
+                    Toast.makeText(baseContext,"Todo Task",Toast.LENGTH_SHORT).show()
+                    return@setOnItemSelectedListener false
+
+                }
+
+
+            }
+            return@setOnItemSelectedListener true
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
